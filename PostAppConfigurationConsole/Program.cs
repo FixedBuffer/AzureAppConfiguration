2c2a3fb4c27bce094689d118a2696aa10383747e
JorTurFer <jorge_turrado@hotmail.es>
2020-01-12 20:43:39 +0100

    Añado ejemplo consola

---
+++ PostAppConfigurationConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace TestConsole
+{
+    internal class Program
+    {
+        private static IConfiguration _configuration = null;
+
+        private static void Main(string[] args)
+        {
+            var connectionString = "Pon aquí tu cadena de conexión";
+
+            var builder = new ConfigurationBuilder();
+            builder.AddAzureAppConfiguration(options =>
+            {
+                options.Connect(connectionString)
+                    .ConfigureRefresh(refresh => //registramos el refresco
+                    {
+                        refresh.Register("asp:welcomesettings:author")
+                                .Register("asp:welcomesettings:message")
+                                .SetCacheExpiration(TimeSpan.FromSeconds(10));
+                    });
+            });
+            _configuration = builder.Build();
+            Console.WriteLine(_configuration["asp:welcomesettings:message"]);
+
+
+            Console.ReadLine();
+        }
+    }
+}
